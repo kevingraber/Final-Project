9c98922bddf5bfcd3da4486db8f9943ce3a57730
kg <kjg310@gmail.com>
2016-07-20 15:24:34 -0400

    user list seperated and some more event style

--- index.android.js
+++ index.android.js
@@ -7,12 +7,11 @@ import {
   Image,
   TextInput,
   Dimensions,
-  Navigator
+  Navigator,
 } from 'react-native';
 import { Router, Scene } from 'react-native-router-flux';
 
 import PageOne from './PageOne.js';
-// import PageTwo from './PageTwo.js';
 import SignUp from './SignUp.js'
 import LogIn from './LogIn.js'
 import LoggedIn from './LoggedIn.js'
@@ -20,14 +19,14 @@ import SearchResults from './SearchResults.js'
 import Result from './Result'
 import CreateEvent from './CreateEvent'
 import UpdateUser from './UpdateUser'
+import UserList from './UserList'
 
 class FinalProject extends Component {
   render() {
     return (
-      <Router>
+      <Router navigationBarStyle={styles.navBar} backButtonTextStyle={styles.backButton}>
         <Scene key="root">
           <Scene key="pageOne" component={PageOne} title="Welcome!" initial={true} />
-          {/*<Scene key="pageTwo" component={PageTwo} title="PageTwo" />*/}
           <Scene key="login" direction="vertical" component={LogIn} title="Log In!" />
           <Scene key="signup" component={SignUp} title="Sign Up!" />
           <Scene key="welcome" component={LoggedIn} title="Welcome!" />
@@ -35,10 +34,22 @@ class FinalProject extends Component {
           <Scene sceneStyle={{paddingTop: Navigator.NavigationBar.Styles.General.NavBarHeight}} key="eventpage" component={Result} title="Event Page!" />
           <Scene key="createevent" component={CreateEvent} title="Create Event!" />
           <Scene key="updateuser" component={UpdateUser} title="Update User" />
+          <Scene sceneStyle={{paddingTop: Navigator.NavigationBar.Styles.General.NavBarHeight}} key="userlist" component={UserList} title="User List" />
         </Scene>
       </Router>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  navBar: {
+    backgroundColor: 'white',
+    borderBottomWidth: 0
+  },
+  backButton: {
+    color: '#c0cac9'
+  }
+});
+
 
 AppRegistry.registerComponent('FinalProject', () => FinalProject);
